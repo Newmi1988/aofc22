1ecf2a9a179ab5a2fddd4f65193c226227712c97
Tobias Newmiwaka <newmi1988@gmail.com>
2022-12-01 22:49:00 +0100

    Day 1: added file reader

---
+++ dayOne/src/main.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+
+fn read_file_buffer(filepath: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let f = File::open(filepath)?;
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        println!("{}", line.unwrap());
+    }
+    Ok(())
+}
+
+fn main() {
+
+    let path = Path::new("./input");
+    read_file_buffer(path);
+}
